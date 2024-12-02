3f6d77290c8cc6949d2170d3859fd8655251e111
onionmccabbage <onionmccabbage@live.com>
2024-12-02 15:52:29 +0000

    calling functions

--- functional/main.py
+++ functional/main.py
@@ -6,17 +6,21 @@
 def validateIsNumeric(v): # we may choose to write a DOCSTRING at the top of the fn
     '''take a value and return True is the value is numeric. 
     Otherwise return False'''
-    try:
-        if(type(v)==int or type(type(v)==float)): # we can say 'and' or 'or'
-            return True
-    except Exception as err:
+    if(type(v)==int or type(v)==float): # we can say 'and' or 'or'
+        return True
+    else:
         return False
 
 # exercise the code
-print( validateIsNumeric(3) )   # True
-print( validateIsNumeric('3') ) # False
-print( validateIsNumeric(3.4) ) # True
-print( validateIsNumeric(-3) )  # True
+r1 = validateIsNumeric(3)
+r2 = validateIsNumeric('3')
+r3 = validateIsNumeric(3.4)
+r4 = validateIsNumeric(-3)
+# code clarity matters, so it may be clearer to break code out like this
+print( r1 ) # True
+print( r2 ) # False
+print( r3 ) # True
+print( r4 ) # True
 
 print(validateIsNumeric.__doc__) # sometimes it's handy to acess the docstring
 
