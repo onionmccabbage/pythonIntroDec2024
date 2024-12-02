f2491d8e8c58a71797ac4dcde1957d63f0acbac8
onionmccabbage <onionmccabbage@live.com>
2024-12-02 16:13:44 +0000

    coding with modular achitecture

--- functional/main.py
+++ functional/main.py
@@ -1,17 +1,10 @@
 # by convention we often call a module 'main' if it is the 
 # entry-point for a collection of Python modules
-
+from util import num0_100
+from util import validateIsNumeric
 # we often choose to encapsulate functionality inside a function
 # the advantage is we can re-use these functions wehenever needed
 # we may choose to pass arguments, and they may have a default value
-def validateIsNumeric(v=0): # we may choose to write a DOCSTRING at the top of the fn
-    '''take a value and return True is the value is numeric. 
-    Otherwise return False'''
-    # if(type(v)==int or type(v)==float): # we can say 'and' or 'or'
-    if type(v) in (int, float): # does the tyoe exist in the tuple (int, float) ? 
-        return True
-    else:
-        return False
     
 def returnWhat():
     '''what does a function return...'''
@@ -29,8 +22,9 @@ print( r1 ) # True
 print( r2 ) # False
 print( r3 ) # True
 print( r4 ) # True
-
 print(validateIsNumeric.__doc__) # sometimes it's handy to acess the docstring
-
 r5 = returnWhat()
 print(r5) # None
+
+# can we make use of our imported function
+print( num0_100() ) # see a random int 0-100
--- functional/util.py
+++ functional/util.py
@@ -5,3 +5,13 @@ def num0_100():
     '''return a random integer between 0-100 inclusive'''
     n = randint(0,100)
     return n
+
+# put this in a separate module then import it
+def validateIsNumeric(v=0): # we may choose to write a DOCSTRING at the top of the fn
+    '''take a value and return True is the value is numeric. 
+    Otherwise return False'''
+    # if(type(v)==int or type(v)==float): # we can say 'and' or 'or'
+    if type(v) in (int, float): # does the tyoe exist in the tuple (int, float) ? 
+        return True
+    else:
+        return False
