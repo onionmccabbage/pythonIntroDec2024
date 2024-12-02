599efab57b7c93e88c041c08888b01d874e65623
onionmccabbage <onionmccabbage@live.com>
2024-12-02 15:23:32 +0000

    example generator

--- using_range.py
+++ using_range.py
@@ -7,4 +7,12 @@ is_even = n in evens
 
 # we may even do maths within a range (this is then called comprehension)
 squares_lists = [i*i for i in range(0,11)] # we now have a list containing squares 0-10
-print(squares_lists)
+print(squares_lists, type(squares_lists))
+
+# The generator syntax
+# a generator is even more eficient than a range/list
+g = (i*i for i in range(0,11)) # we now have a generator (the values are NOT stored in memory)
+print( g, type(g) )
+# we may itersate over the members of the generator
+for i in g:
+    print(i)
