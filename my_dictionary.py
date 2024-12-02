1b497dd77ce7552b77edf1ae26ae991b1f43d884
onionmccabbage <onionmccabbage@live.com>
2024-12-02 11:39:18 +0000

    using string formatting

--- my_dictionary.py
+++ my_dictionary.py
@@ -15,6 +15,13 @@ for k in d:
     print(k) # we have the keys
 
 for (k,v) in d.items(): # NB we MUST say d.items() if we need key and value
-    print(k, v)
+    # print(k, v)
+    print(f'key {k} has value {v}')
 
-
+# string formatting
+s1 = 'menu'
+s2 = 'fish'
+# we may inject any Python onto {}
+print(f'On the {s1} is {s2}') # f'' means format this string
+# other syntax
+print('On the {0} is {1}'.format(s1, s2)) # older syntax
