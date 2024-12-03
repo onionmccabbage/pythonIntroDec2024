2ae0b97e5d66e9e845e920333a1e6ad1c7a0b7be
onionmccabbage <onionmccabbage@live.com>
2024-12-03 12:25:47 +0000

    retrieving text from a file

--- fifo/read_text.py
+++ fifo/read_text.py
@@ -2,7 +2,8 @@ def readTextFromFile():
     '''retrieve some text from a file'''
     fin = open('my_text.txt', 'rt') # 'rt' will read text
     t = fin.read() # retrieve everything from the file
-    return 
+    fin.close()
+    return t
 
 # exercise the code
 retreived = readTextFromFile()
