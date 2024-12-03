5c668466b33b2431a7aac5acdbf3c13044dd4d78
onionmccabbage <onionmccabbage@live.com>
2024-12-03 12:28:00 +0000

    read and readlines

--- fifo/read_text.py
+++ fifo/read_text.py
@@ -1,11 +1,12 @@
 def readTextFromFile():
     '''retrieve some text from a file'''
     fin = open('my_text.txt', 'rt') # 'rt' will read text
-    t = fin.read() # retrieve everything from the file
+    # t = fin.read() # retrieve everything from the file
+    t = fin.readlines() # retrieve everything into a list of strings (each line)
     fin.close()
     return t
 
 # exercise the code
 retreived = readTextFromFile()
-print(retreived)
+print(retreived, type(retreived))
 
