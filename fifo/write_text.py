e5191efa37946bc9c0931fd2773c79137a45c5f2
onionmccabbage <onionmccabbage@live.com>
2024-12-03 12:08:11 +0000

    simple print to file with exception handling

--- fifo/write_text.py
+++ fifo/write_text.py
@@ -1,9 +1,19 @@
+# we may specify an absolute path
+# or you can derive a path from sys.path, sys.cwd etc.
+
 def printToFile(t):
     '''send print output to a text file'''
-    # we need a file access object
-    fout = open('my_text.txt')
-    print( t, file=fout ) # print to the file access object
-
+    try:
+        # we need a file access object
+        # for exclusive file access, use 'xt' which will create the file, or throw an exception
+        fout = open('my_text.txt', 'at') # 'at' will create or append text ('t' is the default)
+        print( t, file=fout ) # print to the file access object
+    # handle specific exceptions
+    except FileNotFoundError as err:
+        print(f'Problem: {err}')
+    # then handle any generic exceptions
+    except Exception as err:
+        print(f'Some other problem occured: {err}')
 
 # exercise the code
 info = 'this is very simple'
