1158001916dc92b3eb03f6c093d2af1c288a0541
onionmccabbage <onionmccabbage@live.com>
2024-12-03 16:22:50 +0000

    sample python 2 code (will not run in Py 3)

---
+++ py2_demo.py
@@ -0,0 +1,7 @@
+# the libraries do not always behave the same
+print 'hello' # no need for () but you can if you like
+
+a = 3.2
+b = 7
+# data-types are preserved in Py 2
+c = b/a # an int divided by a float so answer will be 2
