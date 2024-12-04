d4c796964a96feae806ec4820e0d874a505d7ccf
onionmccabbage <onionmccabbage@live.com>
2024-12-04 10:48:45 +0000

    using __slots__

--- using_classes/class_b.py
+++ using_classes/class_b.py
@@ -3,10 +3,11 @@
 
 class Point:
     '''x and y will specify a point on a plane'''
+    __slots__ = ['__x', '__y'] # a list of the only permitted properties for this class
     def __init__(self, x, y):
         '''property x and y will be numeric values'''
         self.x = x # call x setter method
-        self.y = y # __y is called 'name mangling'
+        self.y = y 
     def __str__(self):
         '''used by any print() calls'''
         return f'Point is at x:{self.__x} and y:{self.__y}'
@@ -21,18 +22,18 @@ class Point:
             self.__x = new_x
         else:
             raise Exception('X must be numemric')
-    @property 
+    @property # CAREFUL only ONE property decorator per function
     def y(self):
         return self.__y
     @y.setter
     def y(self, new_y): 
         '''validate that the incoming new_y is a numeric value'''
         if type(new_y) in (int, float):
-            self.__y = new_y
+            self.__y = new_y # __y is called 'name mangling'
         else:
             raise Exception('Y must be numemric')
 
 if __name__ == '__main__':
     p1 = Point(3,4)
-    p1.__x = False # we CANNOT directly acces the value of p1.__x
+    # p1.__x = False # we CANNOT directly acces the value of p1.__x
     print(p1.x, p1) # p1.x will call the property function 'x'
