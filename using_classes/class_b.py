bdf48c1858bac71e470520527544940baa53de47
onionmccabbage <onionmccabbage@live.com>
2024-12-04 10:30:22 +0000

    using property decorators

--- using_classes/class_b.py
+++ using_classes/class_b.py
@@ -10,8 +10,19 @@ class Point:
     def __str__(self):
         '''used by any print() calls'''
         return f'Point is at x:{self.__x} and y:{self.__y}'
+    # we may declare property decorators to allow changes to property values
+    @property # decorator adds functionality to an existing function
+    def x(self):
+        return self.__x
+    @x.setter
+    def x(self, new_x):
+        '''validate that the incoming new_x is a numeric value'''
+        if type(new_x) in (int, float):
+            self.__x = new_x
+        else:
+            raise Exception('X must be numemric')
 
 if __name__ == '__main__':
     p1 = Point(3,4)
     p1.__x = False # we CANNOT directly acces the value of p1.__x
-    print(p1.__x, p1)
+    print(p1.x, p1) # p1.x will call the property function 'x'
